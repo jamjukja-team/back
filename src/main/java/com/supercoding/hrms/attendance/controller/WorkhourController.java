d5ef92ddb2fe3fcde2f4ff945e7f7c1168f7d35e
Peppone Choi <peppone.choi@gmail.com>
2025-11-24 12:10:33 +0900

    feat: 근로시간 컨트롤러 작성

--- src/main/java/com/supercoding/hrms/attendance/controller/WorkhourController.java
+++ src/main/java/com/supercoding/hrms/attendance/controller/WorkhourController.java
@@ -1,14 +1,12 @@
 package com.supercoding.hrms.attendance.controller;
 
+import com.supercoding.hrms.attendance.domain.Workhour;
 import com.supercoding.hrms.attendance.dto.request.read.ReadWorkhoursRequestDto;
 import com.supercoding.hrms.attendance.dto.response.WorkhourResponseDto;
 import com.supercoding.hrms.attendance.service.WorkhourService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,13 @@ public class WorkhourController {
 
     @GetMapping
     public ResponseEntity<List<WorkhourResponseDto>> getWorkhours(@ModelAttribute ReadWorkhoursRequestDto request) {
+        List<WorkhourResponseDto> responses = workhourService.getWorkhours(request);
+        return ResponseEntity.ok(responses);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<WorkhourResponseDto> getWorkhour(@PathVariable Long id) {
+        WorkhourResponseDto response = workhourService.getWorkhourById(id);
+        return ResponseEntity.ok(response);
     }
 }
